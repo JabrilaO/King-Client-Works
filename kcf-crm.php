bd5705a02f8e06cb884516051dd0ebd0a87a380c
JabrilaO <jking@kingfinances.com>
2014-02-10 21:41:41 -0500

    Create kcf-crm.php

---
+++ kcf-crm.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: King Client Works
+Plugin URI: http://kingfinances.com
+Description: A custom CRM plugin.
+Version: 1.0
+Author: Jabrila King
+Author URI: http://kingfinances.com
+Original code taken from a plugin developed by:
+Author: Sam Wilson
+Author URI: http://samwilson.id.au/
+*/ 
